1044e1abd4dc43f017173586324dbdbbdc039cb4
One June Kang <onejunekang@gmail.com>
2019-12-08 17:18:52 -0500

    Create: starting ProductItem Component

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {} from 'react-materialize';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({});
+
+export default function ProductItem() {
+  const classes = useStyles();
+  return (
+
+  );
+}
